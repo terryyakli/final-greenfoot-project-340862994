5cba4100d029b86d290556fad6c1c96055323110
340862994 <117937102+340862994@users.noreply.github.com>
2022-12-20 20:58:27 -0500

    Added TitleScreen

---
+++ TitleScreen.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Game", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {
+        super(600, 400, 1);
+
+        addObject(titleLabel, 220, 75);
+        prepare();
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    private void prepare()
+    {
+        Label label = new Label ("Use \u2190 and \u2192 to Move", 40);
+        addObject(label,220,257);
+        label.setLocation(297,243);
+        label.setLocation(305,240);
+        Label label2 = new Label ("Press <space> to Start", 40);
+        addObject(label2,249,331);
+        label2.setLocation(326,319);
+        label.setLocation(328,253);
+        label2.setLocation(295,326);
+        label.setLocation(278,251);
+    }
+}
